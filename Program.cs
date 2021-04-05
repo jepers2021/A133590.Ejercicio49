c39faec3468472cd56fced2a169b36130a0d006b
Jorge <jepers2021@gmail.com>
2021-04-04 21:12:17 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace A133590.Ejercicio49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 49");
+            Console.WriteLine("Programa que crea un archivo de texto con la cadena 'Hola mundo'");
+            string nombreArchivo = "holamundo.txt";
+            if(!File.Exists(nombreArchivo))
+            {
+                using(StreamWriter sw = File.CreateText(nombreArchivo))
+                {
+                    sw.WriteLine("Hola mundo");
+                    Console.WriteLine("Archivo creado exitosamente.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ya existe un archivo llamado: {nombreArchivo}");
+            }
+
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+        }
+    }
+}
